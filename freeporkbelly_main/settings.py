3c7ed401422c28e23d951a19459c8cd3f364893e
고대용 <eodyd6564@naver.com>
2020-05-27 16:06:41 +0900

    add apps

--- freeporkbelly_main/settings.py
+++ freeporkbelly_main/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'customer',
+    'order',
+    'product',
+    'support',
 ]
 
 MIDDLEWARE = [
